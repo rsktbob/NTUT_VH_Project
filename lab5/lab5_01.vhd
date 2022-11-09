4b406617ff2096fa71710b76917e0b52bc6f274a
呂凱達 <t110590038@ntut.edu.tw>
2022-11-09 12:13:17 +0800

    create lab5

--- lab5/lab5_01.vhd
+++ lab5/lab5_01.vhd
@@ -23,12 +23,12 @@ begin
 					qo <= di;
 			else
 				if lr_sel='0' then
-					for i in 1 to 7 loop
+					for i in N downto 1 loop
 						qo(i) <= qo(i-1);
 					end loop;
 					qo(0) <= sdi;
 				else
-					for i in 6 downto 0 loop
+					for i in 1 to N-1 loop
 						qo(i) <= qo(i+1);
 					end loop;
 					qo(7) <= sdi;
